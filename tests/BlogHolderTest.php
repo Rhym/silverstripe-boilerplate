99b58c2c1341c1858a64794338a3668388a217e5
Rhym <contact@ryanpotter.co.nz>
2015-07-21 12:06:54 +1200

    Added a .editorconfig, and formatted the JS accordingly.

--- tests/BlogHolderTest.php
+++ tests/BlogHolderTest.php
@@ -3,7 +3,8 @@
 /**
  * Class BlogHolderTest
  */
-class BlogHolderTest extends SapphireTest {
+class BlogHolderTest extends SapphireTest
+{
 
     protected static $fixture_file = 'blog/tests/BlogTest.yml';
 
